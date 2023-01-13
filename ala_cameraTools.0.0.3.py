596a7b7f2eb99abf7b643cc29982a4602cf6143d
Jeffrey( Cheuk) Chung <13912201@student.uts.edu.au>
2023-01-13 21:29:22 +1100

    Separated create camera function and set aspect ratio
    - create camera function is reverted back to the old and set aspect ratio is a separate function with its separate button
    
    - switched order of tabs between DOF stuff and camera settings (More likely to setup camera then apply DOF)

--- ala_cameraTools.0.0.3.py
+++ ala_cameraTools.0.0.3.py
@@ -53,7 +53,10 @@ def setThreebyTwoSettings():
     setResolutionHeight = cmds.setAttr('defaultResolution.height', 720)
     setDeviceAspectRatio = cmds.setAttr('defaultResolution.deviceAspectRatio', 1.5)
     
-
+# creates an AlexaLF camera and sets the film back. Sets the Far Clip PLane to 10,000. Also setting the render settings to HD.
+def createCamera():
+    cameraName = cmds.camera(n = "ShotCamera", horizontalFilmAperture=1.247, verticalFilmAperture=0.702, farClipPlane=100000)
+    setDefaultSettings()
 
 # Sets the camera Aperature made by the pipeline to match an AlexaLF camera. And sets the scene Render Settings to HD.
 def alexaCamera():
@@ -116,7 +119,13 @@ class cameraTools():
         cmds.tabLayout(self.tabs, edit=True, tabLabel=[firstTab, 'Set Up Camera'])
         cmds.separator(h=10)
         cmds.text('PREVIS: Creates an AlexaLF camera', fn='fixedWidthFont')
-        cmds.text('1. Select your aspect ratio for your camera \n 2. Click Create Camera Button to create the camera in the scene\n')
+        cmds.text('Sets the correct render settings')
+        cmds.separator(h=30)
+        cmds.button(label = 'Create Camera', command = 'createCamera()')
+        cmds.separator(h=30)
+        
+        cmds.text('Set Aspect Ratio', fn='fixedWidthFont')
+        cmds.text('1. Select your aspect ratio for your camera \n 2. Click Confirm Aspect Ratio Button to set those settings\n')
         self.CameraOptionMenu = cmds.optionMenu(w = 250, label = "Set Aspect Ratio")
         #add menu item for all values
         cmds.menuItem(label = "4/3")
@@ -125,7 +134,7 @@ class cameraTools():
         cmds.menuItem(label = "3/2")
         cmds.separator(h=30)
     
-        cmds.button(label = 'Create Camera', command = self.CreateCamera)
+        cmds.button(label = 'Confirm Aspect Ratio', command = self.SetAspectRatio)
 
         cmds.separator(h=30)
         cmds.text('LAYOUT: Sets AlexaLF Settings', fn='fixedWidthFont')
@@ -135,29 +144,9 @@ class cameraTools():
         cmds.setParent("..")
 
     
-        #second tab
-   
+        #Second Tab
         secondTab = cmds.columnLayout(adjustableColumn = True)
-        cmds.tabLayout(self.tabs, edit=True, tabLabel=[secondTab, 'Depth of Field Options'])
-        cmds.separator(h=10)
-        cmds.text('Depth of Field: Set DOF Rig', fn='fixedWidthFont')
-        cmds.text('1. Select your camera in the outliner \n 2. Select your object to focus on in the outliner \n 3. Apply DOF by clicking on the button below')
-        cmds.text("\n\nNOTE: if the focused object's coordinates is at the origin, \nthe aim locator will not spawn but DOF will still be applied as usual", fn='smallObliqueLabelFont')
-        cmds.separator(h=30)
-    
-        cmds.button(label = 'Enable DOF', command = 'addDepthofField()')
-        cmds.separator(h=30)
-        cmds.text('Depth of Field: Disable DOF Rig', fn='fixedWidthFont')
-        cmds.text('1. Select your camera in the outliner \n 2. Select your distance dimension on in the outliner \n 3. Disable DOF by clicking on the button below')
-        cmds.text("\n\nNOTE: You need to delete locators manually", fn='smallObliqueLabelFont')
-        cmds.separator(h=30)
-        cmds.button(label = 'Disable DOF', command = 'disableAllDepthofField(getSelectedCameraShape(), getObjecttoFocus())')
-        cmds.setParent("..")
-        
-       
-        #Third Tab
-        thirdTab = cmds.columnLayout(adjustableColumn = True)
-        cmds.tabLayout(self.tabs, edit=True, tabLabel=[thirdTab, 'Camera Settings'])
+        cmds.tabLayout(self.tabs, edit=True, tabLabel=[secondTab, 'Camera Settings'])
         cmds.separator(h=10)
         cmds.text('Set Focal Length of Selected Camera', fn='fixedWidthFont')
         cmds.text('1. Select your camera in the outliner \n 2. Select your focal length in the dropdown menu \n 3. Confirm your settings by clicking on the Confirm Focal Length Button\n')
@@ -201,6 +190,24 @@ class cameraTools():
         cmds.separator(h=30)
         cmds.button(label = "Confirm Locator Scale" , command=self.SetLocatorScale)
         cmds.setParent("..")
+        
+        #Third tab
+        thirdTab = cmds.columnLayout(adjustableColumn = True)
+        cmds.tabLayout(self.tabs, edit=True, tabLabel=[thirdTab, 'Depth of Field Options'])
+        cmds.separator(h=10)
+        cmds.text('Depth of Field: Set DOF Rig', fn='fixedWidthFont')
+        cmds.text('1. Select your camera in the outliner \n 2. Select your object to focus on in the outliner \n 3. Apply DOF by clicking on the button below')
+        cmds.text("\n\nNOTE: if the focused object's coordinates is at the origin, \nthe aim locator will not spawn but DOF will still be applied as usual", fn='smallObliqueLabelFont')
+        cmds.separator(h=30)
+    
+        cmds.button(label = 'Enable DOF', command = 'addDepthofField()')
+        cmds.separator(h=30)
+        cmds.text('Depth of Field: Disable DOF Rig', fn='fixedWidthFont')
+        cmds.text('1. Select your camera in the outliner \n 2. Select your distance dimension on in the outliner \n 3. Disable DOF by clicking on the button below')
+        cmds.text("\n\nNOTE: You need to delete locators manually", fn='smallObliqueLabelFont')
+        cmds.separator(h=30)
+        cmds.button(label = 'Disable DOF', command = 'disableAllDepthofField(getSelectedCameraShape(), getObjecttoFocus())')
+        cmds.setParent("..")
       
         cmds.showWindow(self.win)
         
@@ -214,10 +221,9 @@ class cameraTools():
         menuVal = cmds.optionMenu(self.LocatorScaleOptionMenu, q=True, value=True)
         adjustLocatorScale(float(menuVal))
         
-    # creates an AlexaLF camera and sets the film back. Sets the Far Clip PLane to 10,000. Also setting the render settings to HD.
-    def CreateCamera(self, *args):
+    #adjust aspect ratio 
+    def SetAspectRatio(self, *args):
         menuVal = cmds.optionMenu(self.CameraOptionMenu, q=True, value=True)
-        cameraName = cmds.camera(n = "ShotCamera", horizontalFilmAperture=1.247, verticalFilmAperture=0.702, farClipPlane=100000)
         if menuVal == '4/3':
             setFourbyThreeSettings()
         elif menuVal == '16/9':
