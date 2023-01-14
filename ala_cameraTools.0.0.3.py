0b71a4bf8763abc00464297491f44517fbc52d9c
Jeffrey( Cheuk) Chung <13912201@student.uts.edu.au>
2023-01-14 18:17:04 +1100

    Can apply DOF via f stop
    - created a function to categorise common lines of code for both DOF methods
    
    - added the respective button to apply DOF via f stop
    
    - disable DOF button works for both options by default
    
    -distance between objects will be clamped to 64 units by default for f stop

--- ala_cameraTools.0.0.3.py
+++ ala_cameraTools.0.0.3.py
@@ -82,8 +82,8 @@ def assignLocators(shotCameraTransform, objectToFocus):
          elif cmds.getAttr(locatorTransform + '.translateX') == cmds.getAttr(objectToFocus + '.translateX') and cmds.getAttr(locatorTransform  + '.translateY') == cmds.getAttr(objectToFocus + '.translateY') and cmds.getAttr(locatorTransform + '.translateZ') == cmds.getAttr(objectToFocus + '.translateZ'):
              cmds.rename(locatorTransform, 'AimLocator') #rename locator closer to the object to 'AimLocator'
   
-#add DOF rig         
-def addDepthofField():
+#refactored code common to both DOF methods
+def basicDepthofFieldSettings():
     #get selected camera shape, transform and objec to focus for DOF
     shotCameraShape = getSelectedCameraShape()
     shotCameraTransform = getSelectedCameraTransform()
@@ -95,13 +95,25 @@ def addDepthofField():
     #Use distance tool to create 2 locators between camera and selected object
     distanceDimensionShape = cmds.distanceDimension(sp=(cmds.getAttr(shotCameraTransform + '.translateX'), cmds.getAttr(shotCameraTransform + '.translateY'), cmds.getAttr(shotCameraTransform + '.translateZ')), ep=(cmds.getAttr(objectToFocus + '.translateX'), cmds.getAttr(objectToFocus + '.translateY'), cmds.getAttr(objectToFocus + '.translateZ')))
     assignLocators(shotCameraTransform, objectToFocus)
+    return shotCameraShape, distanceDimensionShape
+    
+
+#add DOF rig via Focal length         
+def addDepthofField():
+    shotCameraShape, distanceDimensionShape = basicDepthofFieldSettings()
     cmds.connectAttr(distanceDimensionShape + '.distance', shotCameraShape+'.focusDistance') #connect distance attribute of distance dimension to focus distance of camera so that DOF can be varied
     #do the same for arnold render view
     cmds.setAttr(shotCameraShape + ".aiEnableDOF", True)
     cmds.setAttr(shotCameraShape + ".aiApertureSize", 2.8)
     cmds.connectAttr(distanceDimensionShape + '.distance', shotCameraShape+'.aiFocusDistance')
+
     
-#disable Depth of Field Rig of camera
+#connect DOF via f stop, distance will be clamped to 64 units if it goes above
+def addDepthofFieldFStop():
+    shotCameraShape, distanceDimensionShape = basicDepthofFieldSettings()
+    cmds.connectAttr(distanceDimensionShape + '.distance', shotCameraShape+'.fStop') #connect distance attribute of distance dimension to f stop of camera so that DOF can be varied
+    
+#disable Depth of Field Rig of camera (focal distance + fStop)
 def disableAllDepthofField(cameraShape, distanceDimensionShape):
     cmds.delete(distanceDimensionShape)
     cmds.setAttr(cameraShape+".depthOfField", False)
@@ -124,10 +136,10 @@ class cameraTools():
         cmds.separator(h=10)
         cmds.text('PREVIS: Creates an AlexaLF camera', fn='fixedWidthFont')
         cmds.text('Sets the correct render settings')
-        cmds.separator(h=30)
+        cmds.separator(h=20)
         cmds.button(label = 'Create Camera', command = 'createCamera()')
-        cmds.separator(h=30)
-        
+        cmds.separator(h=20)
+       
         cmds.text('Set Aspect Ratio', fn='fixedWidthFont')
         cmds.text('1. Select your aspect ratio for your scene \n 2. Click Confirm Aspect Ratio Button to set those settings\n')
         self.CameraOptionMenu = cmds.optionMenu(w = 250, label = "Set Aspect Ratio")
@@ -136,13 +148,13 @@ class cameraTools():
         cmds.menuItem(label = "16/9")
         cmds.menuItem(label = "16/10")
         cmds.menuItem(label = "3/2")
-        cmds.separator(h=30)
+        cmds.separator(h=20)
     
         cmds.button(label = 'Confirm Aspect Ratio', command = self.SetAspectRatio)
 
-        cmds.separator(h=30)
+        cmds.separator(h=20)
         cmds.text('LAYOUT: Sets AlexaLF Settings', fn='fixedWidthFont')
-        cmds.separator(h=30)
+        cmds.separator(h=20)
     
         cmds.button(label = 'AlexaLF Settings', command = 'alexaCamera()')
         cmds.setParent("..")
@@ -172,10 +184,10 @@ class cameraTools():
         cmds.menuItem(label = "100")
         cmds.menuItem(label = "135")
         cmds.menuItem(label = "150")
-        cmds.separator(h=30)
+        cmds.separator(h=20)
         cmds.button(label = "Confirm Focal Length" , command=self.SetFocalLength)
         
-        cmds.separator(h=30)
+        cmds.separator(h=20)
         cmds.text('Set Locator Scale of Selected Camera', fn='fixedWidthFont')
         cmds.text('1. Select your camera in the outliner \n 2. Select your locator scale in the dropdown menu \n 3. Confirm your settings by clicking on the Confirm Locator Scale Button\n')
         self.LocatorScaleOptionMenu = cmds.optionMenu(w = 250, label = "Set Locator Scale (mm)")
@@ -191,7 +203,7 @@ class cameraTools():
         cmds.menuItem(label = '70')
         cmds.menuItem(label = '85')
         cmds.menuItem(label = '100')
-        cmds.separator(h=30)
+        cmds.separator(h=20)
         cmds.button(label = "Confirm Locator Scale" , command=self.SetLocatorScale)
         cmds.setParent("..")
         
@@ -201,15 +213,18 @@ class cameraTools():
         cmds.separator(h=10)
         cmds.text('Depth of Field: Set DOF Rig', fn='fixedWidthFont')
         cmds.text('1. Select your camera in the outliner \n 2. Select your object to focus on in the outliner \n 3. Apply DOF by clicking on the button below')
-        cmds.text("\n\nNOTE: if the focused object's coordinates is at the origin, \nthe aim locator will not spawn but DOF will still be applied as usual", fn='smallObliqueLabelFont')
-        cmds.separator(h=30)
+        cmds.text("\n\nNOTE: \n1. if the focused object's coordinates is at the origin, \nthe aim locator will not spawn but DOF will still be applied as usual", fn='smallObliqueLabelFont')
+        cmds.text('2. For the f stop option, distance will be clamped to 64 units', fn='smallObliqueLabelFont')
+        cmds.separator(h=20)
     
-        cmds.button(label = 'Enable DOF', command = 'addDepthofField()')
-        cmds.separator(h=30)
+        cmds.button(label = 'Enable DOF Focal Length', command = 'addDepthofField()')
+        cmds.separator(h=10)
+        cmds.button(label = 'Enable DOF f Stop', command = 'addDepthofFieldFStop()')
+        cmds.separator(h=20)
         cmds.text('Depth of Field: Disable DOF Rig', fn='fixedWidthFont')
         cmds.text('1. Select your camera in the outliner \n 2. Select your distance dimension on in the outliner \n 3. Disable DOF by clicking on the button below')
         cmds.text("\n\nNOTE: You need to delete locators manually", fn='smallObliqueLabelFont')
-        cmds.separator(h=30)
+        cmds.separator(h=20)
         cmds.button(label = 'Disable DOF', command = 'disableAllDepthofField(getSelectedCameraShape(), getObjecttoFocus())')
         cmds.setParent("..")
       
